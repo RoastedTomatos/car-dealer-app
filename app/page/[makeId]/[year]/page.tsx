f3c6cc53897cbc5a24b9b56a61b57bb1d0b290c6
RoastedTomatos <v.honcharov.08@gmail.com>
2025-02-06 23:09:28 +0200

    Too late to say about delay, this is all that was done in +- 4 hours, after this commit everything is over 7-hours deadline (just want to finish this task)

--- app/page/[makeId]/[year]/page.tsx
+++ app/page/[makeId]/[year]/page.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import { Suspense } from 'react';
+
+const getModels = async (makeId: string, year: string) => {
+  const res = await fetch(
+    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+  );
+  return res.json();
+};
+
+const ModelList = async ({
+  makeId,
+  year,
+}: {
+  makeId: string;
+  year: string;
+}) => {
+  const data = await getModels(makeId, year);
+
+  return (
+    <div>
+      {data.Results.map((model: any) => (
+        <div>{model.Model_Name}</div>
+      ))}
+    </div>
+  );
+};
+
 export default function ResultPage() {
   return <>This is result</>;
 }
